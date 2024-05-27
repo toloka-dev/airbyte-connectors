aeac09427d8b046bdc4a4ef5dad530625199447a
Leonid Kozhinov <lkozhinov@toloka.ai>
2024-05-27 18:47:50 +0200

    Fix global state event serialization

--- airbyte-cdk/python/airbyte_cdk/destinations/destination.py
+++ airbyte-cdk/python/airbyte_cdk/destinations/destination.py
@@ -117,4 +117,4 @@ class Destination(Connector, ABC):
         parsed_args = self.parse_args(args)
         output_messages = self.run_cmd(parsed_args)
         for message in output_messages:
-            print(message.json(exclude_unset=True))
+            print(message.json(exclude_unset=True, by_alias=True))
